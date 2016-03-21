49c4d4181fd39798ebee9d66e7b86abeae248c2b
John Hattori <johnhattori@gmail.com>
2016-03-21 11:15:24 -0700

    build first React component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function () {
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'John Hattori'
+    var friends = ['Luca Pellicoro', 'Luca Nicolini', 'Sam Harnett']
     return (
-      <div> Hello React JS Program </div>
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
     )
   }
 });
 
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
 ReactDOM.render(
-  <HelloWorld />,
+  <FriendsContainer />,
   document.getElementById('app')
 )
