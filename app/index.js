6f1b2a609bff9739563dae93446192e76f8704fc
John Hattori <johnhattori@gmail.com>
2016-03-24 13:05:28 -0700

    create components directory and Home.js and Main.js files

--- app/index.js
+++ app/index.js
@@ -1,35 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var FriendsContainer = React.createClass({
-  render: function(){
-    var name = 'John Hattori'
-    var friends = ['Luca Pellicoro', 'Luca Nicolini', 'Sam Harnett']
-    return (
-      <div>
-        <h3> Name: {name} </h3>
-        <ShowList names={friends} />
-      </div>
-    )
-  }
-});
-
-var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend){
-      return <li> {friend} </li>;
-    });
-    return (
-      <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
-      </div>
-    )
-  }
-});
 ReactDOM.render(
-  <FriendsContainer />,
+  routes,
   document.getElementById('app')
 )
